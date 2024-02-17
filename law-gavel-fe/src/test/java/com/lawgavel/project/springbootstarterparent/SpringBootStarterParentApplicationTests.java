d59132c71b82908da65e44b5819a33f960f37238
Prabhanshu-dev <prabhanshu4@gmail.com>
2024-02-17 14:22:50 +0530

    check

---
+++ law-gavel-fe/src/test/java/com/lawgavel/project/springbootstarterparent/SpringBootStarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawgavel.project.springbootstarterparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
