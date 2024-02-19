368f3c80e474d501b7139ca7ddd300c78b3e488e
Prabhanshu-dev <prabhanshu4@gmail.com>
2024-02-20 00:18:34 +0530

    commit all

---
+++ law-gavel-fe/src/main/java/com/lawgavel/project/springbootstarterparent/controller/lawGavelController.java
@@ -0,0 +1,12 @@
+package com.lawgavel.project.springbootstarterparent.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class lawGavelController {
+   
+    
+    public  Boolean loginUser(UserDetails utd){
+          
+    }
+}
